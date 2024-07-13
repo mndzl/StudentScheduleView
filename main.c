01ec87473c8fa26b1081a159517d0dcf67992d03
Luis Méndez <luismendez@Luiss-MacBook-Air.local>
2024-07-13 14:26:18 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,35 @@
+/*
+    Written by Luis Mendez
+    Date Written: Jul 13, 2024
+    Purpose: 
+        Exam 4 - Student Schedule View
+        Create a program that will keep track of students, their courses, and their schedule.
+
+    ---------- FEATURES ----------
+    * Create/Update/Remove Student
+    * Create/Update/Remove Class
+    * View all students (sorted by last name)
+    * View all courses (sorted by ID)
+    * Search student and view courses
+    * Search class and view amount of students
+    * Menu system
+
+    ------- ERROR PREVENTION -------
+    * Free Students and Course lists
+    
+    ------ OTHER REQUIREMENTS ------
+    * User provides size
+    * Use header file
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    
+
+
+
+    return 0;
+}
